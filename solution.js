5642b9e3bc09c79e1c0a4714632f7a5f22667ee0
Laura Wu <leemaiwu@gmail.com>
2023-04-15 18:29:09 -0600

    Added ternary solution

--- solution.js
+++ solution.js
@@ -6,4 +6,13 @@ const areaOrPerimeter = (l , w) => {
     }
   }
 
-console.log(areaOrPerimeter(6, 10))
+console.log(areaOrPerimeter(3, 3)) // 9
+console.log(areaOrPerimeter(6, 10)) // 32
+
+
+// Alternate Solution
+
+const squareOrRectangle = (l, w) => l === w ? l*w : 2*(l+w)
+
+console.log(squareOrRectangle(3, 3)) // 9
+console.log(squareOrRectangle(6, 10)) // 32
